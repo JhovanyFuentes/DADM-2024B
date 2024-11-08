de73b890d2246603dd3148eba36cc717405f6147
JhovanyFuentes <jhovanyfue@gmail.com>
2024-11-08 16:50:05 +0000

    ⭐S03S04 Componente PlanPicker

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,12 +1,12 @@
 <script setup>
-import planItem from './plan-item.vue';
+import PlanItem from './PlanItem.vue';
 import { ref } from 'vue';
 const plans= ref (['El cafetero', 'El chelero', 'El intelectual'])
 </script>
 
 <template>
 <div class="plans">
-      <plan-item v-for="plan in plans" 
+      <PlanItem v-for="plan in plans" 
       :key="plan"
       :name="plan"/>
     </div>
