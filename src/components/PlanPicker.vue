4aa223ef1f20a9ae25f329fbe35c249984225139
JhovanyFuentes <jhovanyfue@gmail.com>
2024-11-27 17:28:15 +0000

    ⚒️S03S06 Hooks Topic Template references

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -10,18 +10,21 @@ const selectPlan = (name) => {
 
   //console.log(name)
 }
+
+const plansWrapper = ref(null);
+
 //Intentando obtener la referencia al elemento .plans
 console.log("Termina de ejecutar el script setup 🎉")
 
 //Registrando el hook mounted
 onMounted(()=>{
   console.log("El componente PlanPikcker se ha montado")
-  console.log(document.querySelector('plans'))
+  console.log("plansWrapper.value")
 })
 </script>
 
 <template>
-  <div class="plans">
+  <div ref="plansWrapper" class="plans">
     <PlanPickerItem 
     v-for="plan in plans" 
     :key="plan" 
